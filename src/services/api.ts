4ddbafc87cb78edf3dd108315eff8a7e55446be6
Ravi Ranjan Kumar <121680729+RaviRanjanKumar11@users.noreply.github.com>
2026-03-24 23:52:46 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 const API = axios.create({
   baseURL: BASE_URL,
     headers: {
-    "x-api-key": "YOUR_API_KEY"
+    "x-api-key":  localStorage.getItem("apiKey") || ""
   }
 });
 
