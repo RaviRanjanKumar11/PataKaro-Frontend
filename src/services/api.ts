49dc3207ca3b5b33c55a0fc55bdcb31b8e98c7c3
Ravi Ranjan Kumar <121680729+RaviRanjanKumar11@users.noreply.github.com>
2026-03-24 23:27:18 +0530

    Enable credentials for API requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,8 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const API = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+   withCredentials: true
 });
 
 // IFSC Code Search
